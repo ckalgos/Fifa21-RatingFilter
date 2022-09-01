f848af78181be8d6f78e6cc5cd590aa5ffa4785c
Ck Algos <chithakumar13@gmail.com>
2022-09-02 01:51:06 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# Fifa21-RatingFilter [Deprecated]
+
+This script is Deprecated pls use [Fut-trade-enhancer](https://github.com/ckalgos/fut-trade-enhancer) instead
+
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![Total Downloads](https://img.shields.io/github/downloads/ckalgos/Fifa21-RatingFilter/total.svg)]()
+
+<p align="center"> 
+  <h3 align="center">Fifa21 RatingFilter</h3>
+
+  <p align="center">
+    Fifa21 RatingFilter!
+    <br />  
+    <br /> 
+    <a href="https://github.com/ckalgos/Fifa21-RatingFilter/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/ckalgos/Fifa21-RatingFilter/issues">Request Feature</a>
+  .
+  <a href="https://www.youtube.com/channel/UC5eLkjmLU2TcE4oiJM9PsyA?sub_confirmation=1">Subscribe</a> 
+    
+   # Must Read :no_entry_sign:
+  
+  These tool is developed to demonstrate how someone can develop script to break our web application by automating stuffs and only for learning purpose.
+  
+   EA might (soft) ban from using transfer market in web app for using this tool. Continuously soft ban might lead to permanent ban as well. Also use of tools like this to gain advantage over other players is not ethically right.  
+   
+   Use this tool at your own risk, any developers contributing to this repo won’t held responsibility if your account gets banned.
+  </p>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Prerequisites](#prerequisites)
+- [Usage](#Usage)
+- [Demo](#Demo)
+- [Contact](#contact)
+
+<!-- installation -->
+
+## Installation
+
+- Download the latest release from - [Latest Release](https://github.com/ckalgos/Fifa21-RatingFilter/releases/).
+- Add Tamper Monkey Extenstion to your Browser.
+- Navigate to Create Script -> Then to Utilities.
+- Under Utilities -> File (Import the filter.js script and Install it).
+
+Now in Ultimate Team Web App, under Settings Menu , you will have Rating Filter.
+
+<!-- Prerequisites -->
+
+## Prerequisites
+
+- To use this tool, the user should have access to the transfer market.
+- Hence play the required number of games to get access to the transfer market before trying this tool.
+
+<!-- Usage -->
+
+## Usage
+
+- Update chrome to the latest version.
+- Use this link to access web app, https://www.ea.com/fifa/ultimate-team/web-app/
+- For special cards, rating should be same as basic card rating of the player and then select the card type in rarity.
+- For example to search Ansu Fati TOTW, the rating as to be given as 76 and then in rarity select Team of the Week.
+
+<!-- Demo -->
+
+## Demo
+
+![](Demo.gif)
+
+<!-- CONTACT -->
+
+## Contact
+
+Instagram - [@Instagram](https://www.instagram.com/ckalgos/) - ckalgos@gmail.com
+
+Project Link: [https://github.com/ckalgos/Fifa21-RatingFilter](https://github.com/ckalgos/Fifa21-RatingFilter)
+
+<!-- MARKDOWN LINKS & IMAGES -->
+
+[contributors-shield]: https://img.shields.io/github/contributors/ckalgos/Fifa21-RatingFilter.svg?style=flat-square
+[contributors-url]: https://github.com/ckalgos/Fifa21-RatingFilter/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/ckalgos/Fifa21-RatingFilter.svg?style=flat-square
+[forks-url]: https://github.com/ckalgos/Fifa21-RatingFilter/network/members
+[stars-shield]: https://img.shields.io/github/stars/ckalgos/Fifa21-RatingFilter.svg?style=flat-square
+[stars-url]: https://github.com/ckalgos/Fifa21-RatingFilter/stargazers
+[issues-shield]: https://img.shields.io/github/issues/ckalgos/Fifa21-RatingFilter.svg?style=flat-square
+[issues-url]: https://github.com/ckalgos/Fifa21-RatingFilter/issues
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=flat-square&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/ckalgos
---
+++ filter.js
@@ -0,0 +1,394 @@
+// ==UserScript==
+// @name         FUT 21 Filters with TamperMonkey
+// @namespace    http://tampermonkey.net/
+// @version      1.0.4
+// @updateURL    https://github.com/ckalgos/Fifa-AutoBuyer/blob/master/autobuyer.js
+// @description  FUT Snipping Tool
+// @author       CK Algos
+// @match        https://www.ea.com/*/fifa/ultimate-team/web-app/*
+// @match        https://www.ea.com/fifa/ultimate-team/web-app/*
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  window.UTSearchEnhancerViewController = function () {
+    UTMarketSearchResultsViewController.call(this);
+    this._jsClassName = "UTSearchEnhancerViewController";
+  };
+
+  JSUtils.inherits(
+    UTSearchEnhancerViewController,
+    UTMarketSearchResultsViewController
+  );
+
+  var db = openDatabase("filter", "1.0", "FIFA Filter DB", 2 * 1024 * 1024);
+
+  db.transaction(function (tx) {
+    tx.executeSql(
+      "CREATE TABLE IF NOT EXISTS PlayerId (id, nationid,leagueid,teamid)"
+    );
+    tx.executeSql("CREATE TABLE IF NOT EXISTS CurrentRating (rating)");
+  });
+
+  window.currentRating = "";
+
+  db.transaction(function (tx) {
+    tx.executeSql(
+      "SELECT * FROM CurrentRating",
+      [],
+      function (tx, ratingResults) {
+        if (ratingResults.rows.length) {
+          window.currentRating = ratingResults.rows[0].rating || "";
+        }
+      },
+      null
+    );
+  });
+
+  window.getRandNumber = function (min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  };
+
+  UTItemService.prototype.searchTransferMarket = function searchTransferMarket(
+    searchCriteria,
+    pageNum,
+    fromRequestItem,
+    observable
+  ) {
+    var result = observable || new EAObservable();
+    if (!fromRequestItem && pageNum === 1) {
+      this.updateCriteria(searchCriteria, pageNum, result);
+      return result;
+    }
+    var response = new UTServiceResponseDTO();
+    function successSearch(param1, successResult) {
+      param1.unobserve(this);
+      response.success = successResult.success;
+      response.status = successResult.status;
+      response.data = {};
+      response.data.items = successResult.response.items;
+      if (successResult.success && successResult.response.items.length > 0) {
+        this.marketRepository.setPageCache(
+          pageNum,
+          successResult.response.items,
+          Date.now(),
+          successResult.maxAge > 0 ? successResult.maxAge : null
+        );
+      }
+      result.notify(response);
+    }
+    function refreshSuccess(currRefresh, refreshResult) {
+      currRefresh.unobserve(this);
+      response.success = !![];
+      response.status = refreshResult.success
+        ? HttpStatusCode["OK"]
+        : HttpStatusCode.NOT_MODIFIED;
+      response.data = {};
+      response.data.items = this.marketRepository.getPageCache(pageNum);
+      result.notify(response);
+    }
+    if (!this.marketRepository.isCacheExpired(pageNum)) {
+      var currPageCache = this.marketRepository.getPageCache(pageNum);
+      if (currPageCache.length > 0) {
+        this.refreshAuctions(currPageCache).observe(this, refreshSuccess);
+      } else {
+        response.success = !![];
+        response.status = HttpStatusCode.NOT_MODIFIED;
+        response.data = {};
+        response.data.items = currPageCache;
+        result.notify(response);
+      }
+    } else {
+      var currConfigObj = getAppMain()
+        .getConfigRepository()
+        .getConfigObject(EAConfigurationRepository.KEY_ITEMS_PER_PAGE);
+      var currItemCount = JSUtils.isValid(currConfigObj)
+        ? currConfigObj[
+            EAConfigurationRepository.ITEMS_PER_PAGE.TRANSFER_MARKET
+          ]
+        : 20;
+      searchCriteria.offset = currItemCount * (pageNum - 1);
+      searchCriteria.count = currItemCount + 1;
+      accessobjects.Item.searchTransferMarket(searchCriteria).observe(
+        this,
+        successSearch
+      );
+    }
+    return result;
+  };
+
+  UTItemService.prototype.updateCriteria = function updateCriteria(
+    searchCriteria,
+    l,
+    result
+  ) {
+    db.transaction(
+      function (tx) {
+        tx.executeSql(
+          `SELECT id FROM PlayerId WHERE (${searchCriteria.nation} IS -1 OR nationid = ${searchCriteria.nation}) AND
+                                                             (${searchCriteria.league} IS -1 OR leagueid = ${searchCriteria.league}) AND
+                                                             (${searchCriteria.club} IS -1 OR teamid = ${searchCriteria.club})`,
+          [],
+          function (tx, results) {
+            let playerIds = [];
+            for (let i = 0; i < results.rows.length; i++) {
+              playerIds.push(results.rows[i].id);
+            }
+
+            if (playerIds.length) {
+              searchCriteria.maskedDefId =
+                playerIds[getRandNumber(0, playerIds.length - 1)];
+            }
+
+            if (window.currentRating && !searchCriteria.maskedDefId) {
+              searchCriteria.maskedDefId = 1;
+            }
+            this.searchTransferMarket(searchCriteria, l, true, result);
+          }.bind(this)
+        );
+      }.bind(this)
+    );
+  };
+
+  UTMarketSearchResultsViewController.prototype._searchFut = function (l) {
+    this._paginationViewModel.stopAuctionUpdates(),
+      services.Item.searchTransferMarket(this._searchCriteria, l, true).observe(
+        this,
+        function _onRequestItemsComplete(e, t) {
+          if ((e.unobserve(this), !t.success))
+            return NetworkErrorManager.checkCriticalStatus(t.status)
+              ? void NetworkErrorManager.handleStatus(t.status)
+              : (services.Notification.queue([
+                  services.Localization.localize("popup.error.searcherror"),
+                  enums.UINotificationType.NEGATIVE,
+                ]),
+                void con.getNavigationController().popViewController());
+          if (0 < this._searchCriteria.offset && 0 === t.data.items.length)
+            this._requestItems(l - 1);
+          else {
+            var i = this._paginationViewModel.getNumItemsPerPage();
+            var o = t.data.items.slice();
+
+            if (
+              (this.onDataChange.notify({
+                items: o,
+              }),
+              o.length > i && (o = o.slice(0, i)),
+              this._paginationViewModel.setPageItems(o),
+              this._paginationViewModel.setPageIndex(l),
+              this._selectedItem && 0 < o.length)
+            ) {
+              var n = this._paginationViewModel.getIndexByItemId(
+                this._selectedItem.id
+              );
+              0 < n && this._paginationViewModel.setIndex(n),
+                (this._selectedItem = null);
+            }
+            var s = this.getView(),
+              a = null;
+            if (
+              (!this._stadiumViewmodel ||
+                (this._searchCriteria.type !== SearchType.VANITY &&
+                  this._searchCriteria.type !== SearchType.CLUB_INFO &&
+                  this._searchCriteria.type !== SearchType.BALL) ||
+                (a = this._stadiumViewmodel.getStadiumProgression(
+                  this._searchCriteria.subtypes
+                )),
+              s.setItems(this._paginationViewModel.getCurrentPageItems(), a),
+              s.setPaginationState(1 < l, t.data.items.length > i),
+              JSUtils.isValid(this._compareItem) && !this._squadContext)
+            ) {
+              var r = JSUtils.find(
+                o,
+                function (e) {
+                  return (
+                    e.getAuctionData().tradeId ===
+                    this._compareItem.getAuctionData().tradeId
+                  );
+                }.bind(this)
+              );
+              JSUtils.isValid(r)
+                ? this._pinnedListItem.setItem(r)
+                : this._paginationViewModel.setPinnedItem(this._compareItem);
+            } else
+              !isPhone() &&
+                0 < o.length &&
+                s.selectListRow(this._paginationViewModel.getCurrentItem().id);
+          }
+          this._paginationViewModel.startAuctionUpdates();
+        }
+      );
+  };
+
+  window.UTSnipeFilterViewController = function () {
+    UTAppSettingsViewController.call(this);
+    this._jsClassName = "UTSnipeFilterViewController";
+  };
+
+  JSUtils.inherits(UTSnipeFilterViewController, UTAppSettingsViewController);
+
+  window.snipeFilterInterface = function () {
+    if (
+      services.Localization &&
+      jQuery("h1.title").html() ===
+        services.Localization.localize("navbar.label.home")
+    ) {
+      window.hasLoadedAll = true;
+    }
+
+    if (window.hasLoadedAll && jQuery(".ut-app-settings-actions").length) {
+      if (jQuery(".ut-app-settings-actions").first().length) {
+        if (!jQuery("#snipe_rating_filter").length) {
+          jQuery(".ut-app-settings-actions")
+            .first()
+            .append(
+              `<div class="ut-item-search-view">
+                           <div class="search-price-header">
+                                <h1>Snipe Settings:</h1>
+                            </div>
+                            <br/ >
+                            <div style="width:100%" class="price-filter"> 
+                                   <div style="width:100%" class="info">
+                                        <span class="secondary label">Rating:</span>
+                                    </div> 
+                            </div>
+                            <div style="width:100%" class="buttonInfo"> 
+                                    <div class="inputBox">
+                                        <input placeholder="-1" id="ab_player_rating" type="tel" class="numericInput" value=` +
+                window.currentRating +
+                `></div>
+                                    </div> 
+                            </div>
+                            <div style="width:100%" class="button-container"><button id="ab_setting_save" class="btn-standard call-to-action">Save</button></div>
+                 </div>`
+            );
+        }
+      }
+    } else {
+      window.setTimeout(snipeFilterInterface, 1000);
+    }
+  };
+
+  snipeFilterInterface();
+
+  window.notify = function (message, type) {
+    services.Notification.queue([message, type]);
+  };
+
+  window.saveSettings = function () {
+    window.notify(
+      "Saving setting , please wait...",
+      enums.UINotificationType.POSITIVE
+    );
+    jQuery(".ut-click-shield").addClass("showing");
+
+    let playerFieldString = jQuery("#ab_player_rating").val();
+
+    jQuery(".loaderIcon ").css("display", "block");
+    db.transaction(function (tx) {
+      tx.executeSql(
+        `DELETE FROM PlayerId`,
+        [],
+        function (tx, ratingResults) {},
+        null
+      );
+
+      tx.executeSql(
+        `DELETE FROM CurrentRating`,
+        [],
+        function (tx, ratingResults) {},
+        null
+      );
+
+      let rating = parseInt(playerFieldString);
+
+      if (rating) {
+        tx.executeSql(
+          `INSERT INTO CurrentRating (rating) VALUES (${rating})`,
+          [],
+          function (tx, ratingResults) {},
+          null
+        );
+      }
+    });
+
+    setTimeout(function () {
+      window.currentRating = null;
+
+      var rating = parseInt(playerFieldString);
+
+      if (rating) {
+        window.currentRating = rating;
+        window.fetchPlayers(rating);
+      }
+      if (!rating) {
+        jQuery(".ut-click-shield").removeClass("showing");
+        jQuery(".loaderIcon ").css("display", "none");
+        window.notify(
+          "Saved settings successfully",
+          enums.UINotificationType.POSITIVE
+        );
+        return;
+      }
+    }, 1000);
+  };
+
+  window.saveSearch = function () {
+    window.playerJson = JSON.stringify(this._searchCriteria);
+  };
+
+  window.fetchPlayers = function (rating) {
+    jQuery.getJSON(
+      `content/${fut_guid}/${fut_year}/fut/items/web/players.json`,
+      function (res) {
+        let playerFilteredIds = [];
+
+        var legendReduced = res.LegendsPlayers.reduce(function (
+          filtered,
+          option
+        ) {
+          if (rating && option.r === rating) {
+            filtered.push(option.id);
+          }
+          return filtered;
+        },
+        playerFilteredIds);
+
+        var reduced = res.Players.reduce(function (filtered, option) {
+          if (rating && option.r === rating) {
+            filtered.push(option.id);
+          }
+          return filtered;
+        }, playerFilteredIds);
+
+        for (let i = 0; i < playerFilteredIds.length; i++) {
+          services.Item.requestItemByDefId(playerFilteredIds[i]).observe(
+            this,
+            function (sender, response) {
+              let item = response.data.item;
+              db.transaction(function (tx) {
+                tx.executeSql(
+                  `INSERT INTO PlayerId (id,nationid,leagueid,teamid) Values (${playerFilteredIds[i]},${item.nationId},${item.leagueId},${item.teamId})`,
+                  [],
+                  function (tx, ratingResults) {}
+                );
+              });
+
+              if (i === playerFilteredIds.length - 1) {
+                jQuery(".ut-click-shield").removeClass("showing");
+                jQuery(".loaderIcon ").css("display", "none");
+                window.notify(
+                  "Saved settings successfully",
+                  enums.UINotificationType.POSITIVE
+                );
+              }
+            }
+          );
+        }
+      }
+    );
+  };
+
+  jQuery(document).on("click", "#ab_setting_save", saveSettings);
+})();
